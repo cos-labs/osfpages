792af5c4a8fb01df7cdd48f4f7b417e3ddeebff3
Ryan Mason <Rytiggy@gmail.com>
2017-06-30 23:35:40 -0400

    Added ember-power-select

--- app/components/info-layer-settings/component.js
+++ app/components/info-layer-settings/component.js
@@ -1,15 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
-	init() {
-    	this._super(...arguments);
-    	this.errors = [];
-
-  },
-  	didInsertElement() {
-    	this._super(...arguments);
-  },
     actions: {
 	    saveForm() {
 			console.log(JSON.stringify(this.get('layer').settings.properties));
