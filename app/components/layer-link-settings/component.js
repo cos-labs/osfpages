ac93333fc27a30d9b227593524e904f97e3b9d99
Ryan Mason <Rytiggy@gmail.com>
2017-07-03 16:44:11 -0400

    forgot to add components to commit

---
+++ app/components/layer-link-settings/component.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	actions: {
+	    saveForm() {
+			console.log(JSON.stringify(this.get('layer').settings.properties));
+	    }
+  }
+});
