356bcf282a4e7d4a24f52a75987f8f399ff410f3
Caner Uguz <caneruguz@gmail.com>
2017-07-03 22:03:51 -0400

    Improve add layer style

--- app/components/add-layer/component.js
+++ app/components/add-layer/component.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    open: null,
+    isOpen: null,
     actions: {
         addLayer (type) {
-            console.log('add ' + type);
+
         },
         toggleProperty(prop){
             this.toggleProperty(prop);
