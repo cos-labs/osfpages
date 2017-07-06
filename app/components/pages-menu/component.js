bd7daba0221adc115157dba51de6ee73f879f5bb
Caner Uguz <caneruguz@gmail.com>
2017-07-06 15:17:38 -0400

    Add animated scrolling to page navigation and a scroll to top button

--- app/components/pages-menu/component.js
+++ app/components/pages-menu/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        scrollToLayer(index){
+            debugger;
+            let el = $('#layer'+index);
+            let offset = el.offset();
+            $('body').animate({scrollTop:offset.top}, '500');
+        }
+    }
 });
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     editMode: false,
+    actions: {
+        scrollToTop(){
+            $('body').animate({scrollTop:0}, '500');
+        }
+    }
 });
