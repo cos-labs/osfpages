dcb0d9e196799a7283b60a8d9c6e2e96352b71c3
Caner Uguz <caneruguz@gmail.com>
2017-07-11 10:59:51 -0400

    Show users from node and not dummy

--- app/components/layer-info/component.js
+++ app/components/layer-info/component.js
@@ -1,8 +1,26 @@
 import Ember from 'ember';
+import loadAll from 'ember-osf/utils/load-relationship';
 
 export default Ember.Component.extend({
- didReceiveAttrs() {
-    this._super(...arguments);
-  }
-	
+    users: Ember.A(),
+    bibliographicUsers: Ember.A(),
+    getAuthors: function() {
+        // Cannot be called until node has loaded!
+        const node = this.get('node');
+        if (!node) { return };
+        if(this.get('users').length > 0 || this.get('bibliographicUsers').length > 0) { return; }
+        const contributors = Ember.A();
+        loadAll(node, 'contributors', contributors).then(() => {
+            contributors.forEach((item) => {
+                this.get('users').pushObject(item.get('users'));
+                if(item.get('bibliographic')){
+                    this.get('bibliographicUsers').pushObject(item.get('users'));
+                }
+            })
+        });
+    },
+    didReceiveAttrs() {
+        this._super(...arguments);
+        this.getAuthors();
+    }
 });
