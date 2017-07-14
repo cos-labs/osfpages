e1d4ab3fd7617ddbd0623f6e47d65f35642d8180
Ryan Mason <Rytiggy@gmail.com>
2017-07-13 22:11:27 -0400

    Add back removed image-layer and image-layer-text that got removed in merge conflicts

--- app/components/add-layer/component.js
+++ app/components/add-layer/component.js
@@ -64,7 +64,42 @@ export default Ember.Component.extend({
                                 sectionTitle:"Info advanced",
                                 showInNavigation: true,
                                 themeId: 7,
+                           }
+                        }
+                    });
+                    break;
+
+                case 'image': // Update when link is ready
+                    item = Layer.create({
+                        sectionHeader: 'Image',
+                        showInMenu: false,
+                        component: 'layer-image',
+                        settings: {
+                            component: 'layer-image-settings',
+                            properties: {
+                                height: 500,
+                                url: 'http://localhost:4200/img/sample.jpg',
+                                themeId: 3,
                             }
+                        }
+                    });
+                    break;
+                case 'image-text': // Update when link is ready
+                    item = Layer.create({
+                        sectionHeader: 'Image-Text',
+                        showInMenu: true,
+                        component: 'layer-image-text',
+                        settings: {
+                            component: 'layer-image-text-settings',
+                            properties: {
+                                sectionTitle:"Image here ",
+                                showInNavigation: true,
+                                imageUrl: 'http://localhost:4200/img/sample.jpg',
+                                text: 'Lorem ipsum dolor sit amet',
+                                imageOnLeft: true,
+                                textAlignment: 'left',
+                                themeId: 4,
+                                                            }
                         }
                     });
                     break;
