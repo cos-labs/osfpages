b12d71c2bb009172c8a4a7731632d57268b977a6
Caner Uguz <caneruguz@gmail.com>
2017-07-17 09:46:19 -0400

    Add increment

--- app/components/layer-settings/component.js
+++ app/components/layer-settings/component.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        changeSize(direction, item){
+            if(direction === 'bigger') {
+                this.incrementProperty('layer.settings.values.' + item.value, item.incrementSize);
+            }
+            if(direction === 'smaller') {
+                this.decrementProperty('layer.settings.values.' + item.value, item.incrementSize);
+            }
+        }
+    }
 });
--- app/components/layer-title/component.js
+++ app/components/layer-title/component.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    titleStyle: Ember.computed('layer.settings.properties.h1Size', function(){
-        let size = this.get('layer.settings.properties.h1Size') || 30;
+    titleStyle: Ember.computed('layer.settings.values.h1Size', function(){
+        let size = this.get('layer.settings.values.h1Size') || 30;
         return Ember.String.htmlSafe('font-size: ' + size + 'px;');
     })
 });
