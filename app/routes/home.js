f52f76ebd5d178b6aa05691a9368572f5ec2048b
Caner Uguz <caneruguz@gmail.com>
2017-07-17 11:28:23 -0400

    Change data for all layers

--- app/routes/home.js
+++ app/routes/home.js
@@ -76,21 +76,34 @@ let layers = Ember.A([
         component: 'layer-advanced',
         content:'<h3>Click to edit the Advanced Layer</h3><div><img src="http://xxntkd86l336rq5h3k2kbv9l.wpengine.netdna-cdn.com/wp-content/uploads/sites/62/2017/02/cowork-6-1.jpg" style="width: 552px; height: 368.099px;" class="fr-fic fr-fil fr-dii">Mauris imperdiet <strong>ligula a mauris porttitor ultricies.</strong> Praesent ut fringilla orci. Proin feugiat auctor augue non rutrum. Sed ac metus in augue dignissim malesuada non et sem. Pellentesque ut metus odio. Integer fringilla nulla id leo consequat, a sollicitudin sapien fringilla. Fusce vestibulum malesuada nisl. Fusce augue leo, tempus eget mattis vel, imperdiet at nulla.</div>',
         settings: {
-            component: 'layer-advanced-settings',
-            properties: {
+            component: 'layer-settings',
+            values: {
                 sectionTitle:"Advanced",
                 showInNavigation: true,
                 themeId: 5,
-
-            }
+            },
+            form: [
+                {
+                    type: 'text',
+                    label: 'Title text',
+                    value: 'sectionTitle',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show in navigation',
+                    value: 'showInNavigation',
+                    validation: null,
+                },
+            ]
         }
     }),
     Layer.create({
         sectionHeader: 'Info',
         component: 'layer-info',
         settings: {
-            component: 'layer-info-settings',
-            properties: {
+            component: 'layer-settings',
+            values: {
                 sectionTitle:"Info Title",
                 showInNavigation: true,
                 showDescription: true,
@@ -98,62 +111,174 @@ let layers = Ember.A([
                 showBibliographicContributors: false,
                 showAffiliatedInstitutions: true,
                 themeId: 2,
-            }
+            },
+            form: [
+                {
+                    type: 'text',
+                    label: 'Title text',
+                    value: 'sectionTitle',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show in navigation',
+                    value: 'showInNavigation',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show description',
+                    value: 'showDescription',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show contributors',
+                    value: 'showContributors',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show only bibliographic contributors',
+                    value: 'showBibliographicContributors',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show affiliated institutions',
+                    value: 'showAffiliatedInstitutions',
+                    validation: null,
+                }
+            ]
         }
     }),
     Layer.create({
         sectionHeader: 'Link',
         component: 'layer-link',
         settings: {
-            component: 'layer-link-settings',
-            properties: {
+            component: 'layer-settings',
+            values: {
                 sectionTitle:"Link Title",
                 showInNavigation: true,
                 sectionDescription:"this link goes to a place",
                 sectionLink:"www.example.com",
                 themeId: 5,
-            }
+            },
+            form: [
+                {
+                    type: 'text',
+                    label: 'Title text',
+                    value: 'sectionTitle',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show in navigation',
+                    value: 'showInNavigation',
+                    validation: null,
+                },
+                {
+                    type: 'text',
+                    label: 'Link address',
+                    value: 'sectionLink',
+                    validation: null,
+                },
+            ]
         }
     }),
     Layer.create({
         sectionHeader: 'Image',
         component: 'layer-image',
         settings: {
-            component: 'layer-image-settings',
-            properties: {
+            component: 'layer-settings',
+            values: {
                 height: 500,
                 url: 'http://localhost:4200/img/sample.jpg',
                 themeId: 3,
-            }
+            },
+            form: [
+                {
+                    type: 'text',
+                    label: 'Image link',
+                    value: 'url',
+                    validation: null,
+                },
+                {
+                    type: 'increment',
+                    label: 'Section height',
+                    value: 'height',
+                    validation: null,
+                }
+            ]
         }
     }),
     Layer.create({
         sectionHeader: 'Download',
         component: 'layer-file',
         settings: {
-            component: 'layer-file-settings',
-            properties: {
+            component: 'layer-settings',
+            values: {
                 sectionTitle:"Download this file",
                 sectionDescription:"",
                 showFileviewer: true,
                 downloadLink: '',
                 showInNavigation: true,
                 themeId: 6,
-            }
+            },
+            form: [
+                {
+                    type: 'text',
+                    label: 'Title text',
+                    value: 'sectionTitle',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show in navigation',
+                    value: 'showInNavigation',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show file viewer',
+                    value: 'showFileviewer',
+                    validation: null,
+                }
+            ]
         }
     }),
     Layer.create({
         sectionHeader: 'Wiki example',
         component: 'layer-wiki',
         settings: {
-            component: 'layer-wiki-settings',
+            component: 'layer-settings',
             wikiId: '',
-            properties: {
+            values: {
                 sectionTitle:"Wiki example",
                 showInNavigation: true,
                 addShowMore: false,
                 themeId: 5,
-            }
+            },
+            form: [
+                {
+                    type: 'text',
+                    label: 'Title text',
+                    value: 'sectionTitle',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Show in navigation',
+                    value: 'showInNavigation',
+                    validation: null,
+                },
+                {
+                    type: 'checkbox',
+                    label: 'Add show more',
+                    value: 'addShowMore',
+                    validation: null,
+                }
+            ]
         }
     })
 ]);
