56a1892ff7b48207d8d28fa16cabc1665a92dc93
Ryan Mason <Rytiggy@gmail.com>
2017-07-17 15:31:49 -0400

    Add link validation

--- app/components/layer-settings/component.js
+++ app/components/layer-settings/component.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     file_object: null,
+
+    didRender() {
+        this._super(...arguments);
+        const str = this.get('layer.settings.values.sectionLink');
+        if(str !== undefined){
+            this.send('isUrlValid' , str);
+        }
+    },
     actions: {
         changeSize(direction, item){
             if(direction === 'bigger') {
@@ -19,6 +27,18 @@ export default Ember.Component.extend({
         },
         saveForm(){
 
+        },
+        isUrlValid(userInput) {
+            var regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
+            var url = new RegExp(regexQuery,"i");
+            if (url.test(userInput)) {
+                if(userInput.split(".").length > 2){
+                    console.log('valid url: ' + userInput);
+                }
+                return true;
+            }
+            console.log('invalid url: ' + userInput);
+            return false;
         }
     }
 });
