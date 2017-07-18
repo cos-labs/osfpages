ffc8fcba009b8e7c91e33b251a066fcbe52f2acb
Ryan Mason <Rytiggy@gmail.com>
2017-07-18 14:43:40 -0400

    Load intelligent defaults with file widget

--- app/components/layer-file/component.js
+++ app/components/layer-file/component.js
@@ -2,6 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     showSelect: false,
+    didRender() {
+        if(!this.get('layer.settings.values.downloadLink')){
+            this.get('node.files').then((result)=>{
+                result.objectAt(0).get('files').then((files)=>{
+                    let fileDatesLinks = {}
+                    let fileModifiedDates = []
+                    for(let i = 0; i < files.length; i++){
+                        fileModifiedDates.push(files.objectAt(i).get('dateModified'));
+                        fileDatesLinks[files.objectAt(i).get('dateModified')] = files.objectAt(i).get('links').download;
+                    }                
+                    let mostRecentDate = new Date(Math.max.apply(null,fileModifiedDates));
+                    this.set('layer.settings.values.downloadLink' , fileDatesLinks[mostRecentDate]);
+                });
+            });
+        }
+
+    },
     actions: {
         fileDetail(file) {
             this.set('showSelect', false);
