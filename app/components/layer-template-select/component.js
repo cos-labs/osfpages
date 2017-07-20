0385a33a36f736cd75db640bb2a0dde4a56f27fe
Ryan Mason <Rytiggy@gmail.com>
2017-07-20 14:35:52 -0400

    fix typos, fix indentation

---
+++ app/components/layer-template-select/component.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    isOpen: null,
+    actions: {
+        renderTemplet(type) {
+            let self = this;
+            let theme;
+            switch (type) {
+                case 'all':
+                    theme = 'theme_1.json'
+                    break;
+                case 'wiki':
+                    theme = 'theme_2.json'
+                    break;
+                case 'file':
+                    theme = 'theme_3.json'
+                    break;
+                case 'people':
+                    theme = 'theme_4.json'
+                    break;
+            }
+
+            $.ajax({
+                type: 'GET',
+                url: '/themes/' + theme,
+                async: false,
+                success: function(data) {
+                    self.set('layers', data)
+                }
+            });
+        },
+        toggleProperty(prop) {
+            this.toggleProperty(prop);
+        }
+    }
+});
