d612e6cc867a176c9b7f54a2439ca3963c899e30
Caner Uguz <caneruguz@gmail.com>
2017-07-24 16:46:19 -0400

    Add rudimentary scrolling to moved layer down move has issues with certain layers like wiki and download

--- app/components/layer-settings/component.js
+++ app/components/layer-settings/component.js
@@ -8,6 +8,11 @@ export default Ember.Component.extend({
     }),
     backgroundPalette: ['#9e9e9e', '#eeeeee', '#009688', '#00BCD4', '#ffffff', '#31708f', '#f07057'],
     colorPalette: ['#f8f8f8', '#333333', '#ebebeb', '#ffffff', '#f5f5f5'],
+    scrollToLayer(index){
+        let el = $('#layer'+index);
+        let offset = el.offset();
+        $('body').animate({scrollTop:offset.top}, '500');
+    },
     actions: {
         changeSize(direction, item){
             if(direction === 'bigger') {
@@ -26,12 +31,15 @@ export default Ember.Component.extend({
             let removed = layers.objectAt(index);
             layers.insertAt(index-1, removed);
             layers.removeAt(index+1);
+            this.scrollToLayer(index-1);
         },
         moveAfter(index){
             let layers = this.get('layers');
             let removed = layers.objectAt(index);
             layers.insertAt(index+2, removed);
             layers.removeAt(index);
+            this.scrollToLayer(index+1);
+
         },
         // Remove layer
         showRemove(){
@@ -55,6 +63,5 @@ export default Ember.Component.extend({
         toggleCheck(check){
             this.toggleProperty('layer.settings.values.' + check.value);
         }
-
     }
 });
