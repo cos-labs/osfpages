d1cd014518b0e5296f96a8d6b73fd0e9091aa80b
Caner Uguz <caneruguz@gmail.com>
2017-07-25 12:48:54 -0400

    Optimize themes

--- app/components/layer-template-select/component.js
+++ app/components/layer-template-select/component.js
@@ -1,32 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    themeList: [
+        {
+            id: 1,
+            name: 'All',
+            thumb: '/img/all.png'
+        },
+        {
+            id: 2,
+            name: 'Wiki',
+            thumb: '/img/wiki.png'
+        },
+        {
+            id: 3,
+            name: 'File',
+            thumb: '/img/file.png'
+        },
+        {
+            id: 4,
+            name: 'Team',
+            thumb: '/img/people.png'
+
+        },
+    ],
 	isOpen: null,
 	actions: {
-		renderTemplate(type) {
+		renderTemplate(id) {
 			let self = this;
-			let theme;
-			switch (type) {
-				case 'all':
-					theme = 'theme_1.json'
-					break;
-				case 'wiki':
-					theme = 'theme_2.json'
-					break;
-				case 'file':
-					theme = 'theme_3.json'
-					break;
-				case 'people':
-					theme = 'theme_4.json'
-					break;
-			}
+			let theme = `theme_${id}.json`;
+			console.log(theme);
 
 			$.ajax({
 				type: 'GET',
 				url: '/themes/' + theme,
 				async: false,
 				success: function(data) {
-					self.set('layers', data);
+					self.set('theme', data);
 					self.set('isOpen', false);
 				}
 			});
