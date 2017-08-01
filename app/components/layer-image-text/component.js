11d33fee7a83c95f7d3eefed3d90a4c9f9645630
Ryan Mason <Rytiggy@gmail.com>
2017-08-01 14:39:18 -0400

    fix color of theme and add to observer

--- app/components/layer-image-text/component.js
+++ app/components/layer-image-text/component.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	imageOnLeft: Ember.observer('layer.settings.values.alignment', function() {
+	imageOnLeft: Ember.observer('layer.settings.values.alignment', 'editMode', function() {
 		if(this.get('layer.settings.values.alignment') === 'center'){
 			$('textarea').addClass('side-description')
 		}else{
