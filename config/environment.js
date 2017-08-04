5a65196c850cab475a3ceaf98eaa1bc2bd4bef14
Ryan Mason <Rytiggy@gmail.com>
2017-08-04 14:42:56 -0400

    change rootURL

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,7 @@ module.exports = function(environment) {
 
     if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/osfpages-admin/';
+    ENV.rootURL = '/osfpages/';
 
     }
 
