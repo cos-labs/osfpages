fbbbea8df83a23cc6801183237edfb0ed46c8e92
Ryan Mason <Rytiggy@gmail.com>
2017-08-11 15:46:39 -0400

    Add emberfire (firebase), remove use of osfStorage due to 6 second load times, use firebase for storage

--- app/routes/home.js
+++ app/routes/home.js
@@ -19,36 +19,36 @@ function getToken() {
     }
 }
 
-function httpRequest(method, url , params, node , resolve){
+// function httpRequest(method, url , params, node , resolve){
 
-    const deferred = Ember.RSVP.defer();
+//     const deferred = Ember.RSVP.defer();
 
-    let xhttp = new XMLHttpRequest();
-    xhttp.open(method, url, true);
-    xhttp.withCredentials = false;
-    let token = getToken();
-    xhttp.setRequestHeader('Authorization', `Bearer ${token}`);
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.open(method, url, true);
+//     xhttp.withCredentials = false;
+//     let token = getToken();
+//     xhttp.setRequestHeader('Authorization', `Bearer ${token}`);
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200 ) {
-            const content = Ember.Object.create(JSON.parse(this.responseText));
-            const timeMachine = TimeMachine.Object.create({ content });
-            theme = timeMachine;
-            
-            if(resolve){
-                deferred.resolve({
-                    theme,
-                    guid: params.guid,
-                    node: node
-                });
-            }else{
-                deferred.resolve(JSON.parse(this.responseText));
-            }
-        }
-    };
-    xhttp.send();
-    return deferred.promise;
-}
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200 ) {
+//             const content = Ember.Object.create(JSON.parse(this.responseText));
+//             const timeMachine = TimeMachine.Object.create({ content });
+//             theme = timeMachine;
+//             console.log(this.responseText)
+//             if(resolve){
+//                 deferred.resolve({
+//                     theme,
+//                     guid: params.guid,
+//                     node: node
+//                 });
+//             }else{
+//                 deferred.resolve(JSON.parse(this.responseText));
+//             }
+//         }
+//     };
+//     xhttp.send();
+//     return deferred.promise;
+// }
 
 
 export default Ember.Route.extend({
@@ -56,59 +56,57 @@ export default Ember.Route.extend({
         controller.set('model' , model);
     },
     model: async function(params){
-        //Slow load times because OSF doesnt have filters on materializedPath and we cant filter on queryRecord  which would be faster.
-            // this.store.queryRecord('file', {'filter': {'materializedPath':'/OSFPages/data.json'}})
 
-        // let putMessage = await httpRequest('PUT' , `${ENV.OSF.waterbutlerUrl}v1/resources/${params.guid}/providers/osfstorage/?kind=folder&name=OSFPages`)
-        // if(putMessage == 409){
-
-
-            // var data = {
-            //     guid: '5989b2e58ca57e0258f301b5'
-            // };
-            // var record = this.store.createRecord('home', data);
-            
-            // record.save()
+        let defaultJSON ='';
+        $.ajax({
+            type: "GET",
+            url: ENV.rootURL + "themes/theme_1.json",
+            async: false,
+            success: function (data) {
+                defaultJSON = data;
+            }});
 
 
 
-            console.log(this.store.findAll('home'))
 
-            let firebaseDB = this.store.findAll('home');
+        let firebaseDB = this.store.findRecord('home', params.guid)
 
-            let dataGUID = await firebaseDB.then((files)=>{return files.objectAt(0).get('guid')})
-        // // If testing and parameter is not working use this 'jyu4t' for params.guid
+
+
+        console.log(firebaseDB)
+        let jsonBlob = await firebaseDB.then((file)=>{        
+            return  file.get('guid')
+        },function(reason) {
+            return false;
+
+        });
+
+        // If testing and parameter is not working use this 'jyu4t' for params.guid
         let node = await this.store.findRecord('node', params.guid)
-
-
-        // //  //get the json file from the project if it doesnt exist make a new folder 
-        // let folder = await node.get('files')
-        // let OSFStorage = await folder.objectAt(0).get('files')
         
-        // let OSFPages = ''
-        // if( OSFStorage.find(function(child){return child.get('name') === 'OSFPages'})){
-        //     OSFPages = OSFStorage.find(function(child){return child.get('name') === 'OSFPages'})
-        // }else{
-        //     httpRequest('PUT' , `${ENV.OSF.waterbutlerUrl}v1/resources/${params.guid}/providers/osfstorage/?kind=folder&name=OSFPages`)
-        //     OSFPages = OSFStorage.find(function(child){return child.get('name') === 'OSFPages'})
 
-        // }
-        // let osfPagesChildren = await OSFPages.get('files')//
-        //  // let assets = osfPagesChildren.find(function(child){return child.get('name') === 'assets'})
-        //  // let assetsChildren = await assets.get('files')
+        if(!jsonBlob){
+            console.log('in blob lets add this guid' )
+            //add to firebase  
+            var guid = {
+                id: params.guid,
+                guid: JSON.stringify(defaultJSON) 
+            };
+            var record = this.store.createRecord('home', guid);
+            record.save()
 
-
-        //  let dataJSON = osfPagesChildren.find(function(child){return child.get('name') === 'data.json'})
-         //dataJSON.get('links').download+"?direct="
+        }
 
 
-         let JSONData = await httpRequest('GET' , `${ENV.OSF.waterbutlerUrl}v1/resources/${params.guid}/providers/osfstorage/${dataGUID}?direct=` , params , node, true );
+        const content = Ember.Object.create(JSON.parse(jsonBlob));
+        const timeMachine = TimeMachine.Object.create({ content });
+        theme = timeMachine;
 
-
-
-         return JSONData;
-     // }else{
-     //    httpRequest('PUT' , `${ENV.OSF.waterbutlerUrl}v1/resources/${params.guid}/providers/osfstorage/OSFPages?kind=folder&name=assets`)
-     // }
- }
+        console.log(theme)
+        return {
+            theme,
+            guid: params.guid,
+            node: node
+        };
+    }
 });
