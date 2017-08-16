c200595398855f16305c7d84884e6a5642b67852
Ryan Mason <Rytiggy@gmail.com>
2017-08-16 16:44:36 -0400

    add publish button and remove auto save

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,8 @@
 /*global $:true*/
 
 import Ember from 'ember';
+import TimeMachine from 'ember-time-machine';
+
 
 export default Ember.Controller.extend({
     queryParams: ['editMode'],
@@ -10,25 +12,44 @@ export default Ember.Controller.extend({
     }),
     saving: Ember.observer('editMode', function(){
         $(document).ready(()=>{
-            if(this.get('editMode')){
-                this.set('timer', setInterval(()=>{
-                    this.store.findRecord('home', this.get('model.guid')).then((data)=> {
-                        data.set('pageData', JSON.stringify(this.get('model.theme.content')));
-                        data.save();
-                    });
-                }, 5000));
+            let firebaseDB = this.store.findRecord('home', this.get('model.guid'))
+
+            if(this.get('editMode')){//AND CHECK IF THE MODEL ISDIRTY 
+
+                //get unpublishedPageData
+                firebaseDB.then((record)=>{ 
+                    let unpublishedPageData = record.get('unpublishedPageData');
+                    this.set('model.theme.content' , JSON.parse(record.get('unpublishedPageData')))  
+                });
+
+
+
+
+               // this.set('timer', setInterval(()=>{
+
+                    // this.store.findRecord('home', this.get('model.guid')).then((data)=> {
+                    //     console.log(this.get('model.theme.content'))
+                    //     data.set('unpublishedPageData', JSON.stringify(this.get('model.theme.content')));
+                    //     data.save();
+                    // });
+                //}, 5000));
             }else{
                 window.clearInterval(this.get('timer'));
+                firebaseDB.then((record)=>{ 
+                    let pageData = record.get('pageData');
+                    this.set('model.theme.content' , JSON.parse(record.get('pageData')))  
+                });
             }
 
         }); 
-   }),
+    }),
     editMode: false,
     actions: {
         scrollToTop(){
             $('body').animate({scrollTop:0}, '500');
         },
         toggleEditMode(){
+            this.send('save' , this.get('model.guid') )
             this.toggleProperty('editMode');
         },
         undo(){
@@ -39,6 +60,21 @@ export default Ember.Controller.extend({
         redo(){
             if(this.get('model.theme').get('canRedo')) {
                 this.get('model.theme').redo();
+            }
+        },
+        publish(){//publish will take unplblished data and put it in to page data on firebase 
+            this.send('save' , this.get('model.guid') )
+            this.store.findRecord('home', this.get('model.guid')).then((data)=> {               
+                data.set('pageData',  data.get('unpublishedPageData'));
+                data.save();
+            });
+        },
+        save(guid){
+            if(this.get('editMode')){
+               this.store.findRecord('home', this.get('model.guid')).then((data)=> {
+                    data.set('unpublishedPageData', JSON.stringify(this.get('model.theme.content')));
+                    data.save();
+                });
             }
         }
     },
--- app/models/home.js
+++ app/models/home.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	pageData: DS.attr('string')
+	pageData: DS.attr('string'),
+	unpublishedPageData: DS.attr('string')
 });
