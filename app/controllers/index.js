0aa43d3995072c1c76588e6dbc5b81b1cdd9db1a
Caner Uguz <caneruguz@gmail.com>
2017-08-21 11:31:55 -0400

    Move index-select code back in

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import ENV from '../../config/environment';
 import OsfTokenLoginControllerMixin from 'ember-osf/mixins/osf-token-login-controller';
 
 export default Ember.Controller.extend(OsfTokenLoginControllerMixin, {
     session: Ember.inject.service(),
+    didStyle: 'background: #FFC107 url('+ ENV.rootURL+'img/bg.png) no-repeat center; background-size: cover'
 });
