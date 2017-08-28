d19dfcbd899841366d4a205fe7e9436202019d96
Ryan Mason <Rytiggy@gmail.com>
2017-08-28 12:32:22 -0400

    remove comments

--- app/adapters/home.js
+++ app/adapters/home.js
@@ -17,14 +17,12 @@ function getToken() {
 export default DS.JSONAPIAdapter.extend({
     session: Ember.inject.service(),
     host: ENV.apiBaseUrl,
-    // namespace: 'api',
     ajax(url, method, hash) {
         hash = hash || {};
         hash.crossOrigin = true;
         hash.xhrFields = { withCredentials: false };
         hash.headers = hash.headers || {};
         hash.headers['AUTHORIZATION'] = 'Bearer ' + getToken();
-        //hash.headers['X-CSRFTOKEN'] = this.get('session.data.authenticated.csrfToken');
         return this._super(url, method, hash);
     },
     buildURL() {
