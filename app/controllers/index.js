da5d53317b6c2ba27365406254c05aa96a5c0b23
Ryan Mason <Rytiggy@gmail.com>
2017-08-30 09:45:59 -0400

    fix index page start a  project button not working

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,28 @@ import ENV from '../config/environment';
 import OsfTokenLoginControllerMixin from 'ember-osf/mixins/osf-token-login-controller';
 
 export default Ember.Controller.extend(OsfTokenLoginControllerMixin, {
-    starterGuid: null,
-    session: Ember.inject.service(),
-    didStyle: 'background: #FFC107 url('+ ENV.rootURL+'img/bg.png) no-repeat center; background-size: cover'
+	starterGuid: null,
+	showErrorMessage: false,
+	session: Ember.inject.service(),
+	didStyle: 'background: #FFC107 url('+ ENV.rootURL+'img/bg.png) no-repeat center; background-size: cover',
+	actions: {
+		createPage(guid){
+			console.log('this is cool', this.get('starterGuid'))
+			
+			//check to see if the page exists 
+			this.store.findRecord('node', this.get('starterGuid')).then((record)=>{  
+				this.transitionTo('home', this.get('starterGuid'));
+			},(e)=> {
+				console.log(e)
+				this.set('showErrorMessage', true)
+				
+				setTimeout(()=>{
+					this.set('showErrorMessage', false)
+				}, 2000);
+			});
+
+
+		}
+
+	}
 });
