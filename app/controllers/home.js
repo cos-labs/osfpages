e6c534ae55469136f53578e6df96ff4878809ed1
Ryan Mason <Rytiggy@gmail.com>
2017-08-31 11:28:38 -0400

    change to redirect from changing the osf project

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -65,13 +65,6 @@ export default Ember.Controller.extend({
     }),
     editMode: false,
     actions: {
-        makeProjectPublic: function(node){
-            node.set('public' , true)
-            node.save();
-            this.set('isOpen', !node.get('public') )
-
-
-        },
         canUserEdit(){
             if(!this.get('model.node.currentUserPermissions').includes('admin')){
                 this.set('editMode' , false)
