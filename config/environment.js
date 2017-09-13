6af5650ea52a6aaebeeac77756ae948d313e6b69
Ryan Mason <Rytiggy@gmail.com>
2017-09-13 12:44:32 -0400

    update env

--- config/environment.js
+++ config/environment.js
@@ -42,8 +42,8 @@ module.exports = function(environment) {
     }
 
     if (environment === 'prod') {
-        ENV.locationType = 'hash';
-        ENV.rootURL = '/osfpages/';
+        // ENV.locationType = 'hash';
+        // ENV.rootURL = '/osfpages/';
         ENV.osfHostUrl = 'https://api.osf.io';
 
 
